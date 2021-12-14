cdd128474ccef9707979c0b21aeb6b383d2b9ea1
RichardDev01 <60653249+RichardDev01@users.noreply.github.com>
2021-12-14 18:25:03 +0100

    Init push

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Gym-Double-Deep-Q-learning
-DDQ-Learning
+# Adaptive-Systems-2021 Double Deep Q-Learning in OpenAI-gym
+
+### Installation of packages
+To install the package run the following command: 
+```bash 
+pip install .
+```
+
+If you are a developer you need to run this command:
+```bash 
+pip install .[dev]
+```
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+install_requires = [
+    'numpy',
+]
+
+extras = {
+    "dev": [
+        'flake8',
+        'flake8-blind-except',
+        "flake8-builtins",
+        "flake8-docstrings",
+        "flake8-logging-format",
+    ]
+}
+
+setuptools.setup(
+    name='Gym Double Deep Q-learning',
+    description='School work on reinforcement learning',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    extras_require=extras,
+    python_requires='>=3.8',
+    keyword=['Reinforcement Learning', 'schoolwork'],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+)
