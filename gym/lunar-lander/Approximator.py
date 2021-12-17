4ab024b96e982c04d2dd29b46b29d0d2888b605d
RichardDev01 <60653249+RichardDev01@users.noreply.github.com>
2021-12-17 10:35:11 +0100

    Created function to create 2 neural network for double Q learning

--- gym/lunar-lander/Approximator.py
+++ gym/lunar-lander/Approximator.py
@@ -1,4 +1,9 @@
-"""Schrijf een function approximator class. Dit is een neuraal netwerk.
+"""file containing the class for saving, loading, creating and training a Double Q Neural Network."""
+
+from torch import nn
+
+"""
+Schrijf een function approximator class. Dit is een neuraal netwerk.
 Gebruik hiervoor een library naar keuze. De agent heeft twee instanties van approximators,
 een policy-network en een target-network.
 Begin met een Adam Optimizer met een learning rate van 0.001, RMS loss, en 2 hidden layers met 32 neuronen.
@@ -12,20 +17,88 @@ De class heeft de volgende functionaliteit:
 
 
 class Approximator:
+    """Class for saving, loading, creating and training a Double Q Neural Network."""
+
     def __init__(self):
-        pass
+        """Create class variables for double deep learning."""
+        self.q_network_1 = None
+        self.q_network_2 = None
 
     def save_network(self):
+        """Save network."""  # TODO
         pass
 
     def load_network(self):
+        """Load network."""  # TODO
         pass
 
     def train_network(self):
+        """Train network."""  # TODO
         pass
 
     def set_weights(self):
+        """Set weights."""  # TODO
         pass
 
     def load_weights(self):
+        """Load weights."""  # TODO
         pass
+
+    def create_network_q1(self, input_size: int = 8, output_size: int = 4, middle_layer_size: int = 12):
+        """
+        Create first neural network for the double Q learning.
+
+        The network is build out of 4 layers in total from
+        which 2 are hidden. This network is the primary network.
+
+        :param input_size: Input size for the first layer
+        :param output_size: Output size for the last layer
+        :param middle_layer_size: Middle layer size of the first hidden layer
+        """
+        # Check if middle layer is smaller then input size and correct it
+        if middle_layer_size < input_size:
+            middle_layer_size = int(round(input_size * 1.5))
+            print(
+                f"Middle layer is smaller then input size and this is not ideal. check middle layer to size {middle_layer_size} ")
+
+        # Calculate the output size of the 2e middle layer to be a bit smaller then the previous
+        middle_layer_output = int(round(middle_layer_size // 1.25))
+
+        self.q_network_1 = nn.Sequential(
+            nn.Linear(input_size, middle_layer_size),
+            nn.ReLU(),
+            nn.Linear(middle_layer_size, middle_layer_output),
+            nn.ReLU(),
+            nn.Linear(middle_layer_output, output_size),
+        )
+
+    def create_network_q2(self, input_size: int = 8, output_size: int = 4, middle_layer_size: int = 12):
+        """
+        Create second neural network for the double Q learning.
+
+        The network is build out of 4 layers in total from
+        which 2 are hidden. This network is the target network
+
+        :param input_size: Input size for the first layer
+        :param output_size: Output size for the last layer
+        :param middle_layer_size: Middle layer size of the first hidden layer
+        """
+        # Check if middle layer is smaller then input size and correct it
+        if middle_layer_size < input_size:
+            middle_layer_size = int(round(input_size * 1.5))
+            print(
+                f"Middle layer is smaller then input size and this is not ideal. check middle layer to size {middle_layer_size} ")
+
+        # Calculate the output size of the 2e middle layer to be a bit smaller then the previous
+        middle_layer_output = int(round(middle_layer_size // 1.25))
+
+        self.q_network_2 = nn.Sequential(
+            nn.Linear(input_size, middle_layer_size),
+            nn.ReLU(),
+            nn.Linear(middle_layer_size, middle_layer_output),
+            nn.ReLU(),
+            nn.Linear(middle_layer_output, output_size),
+        )
+
+    def get_network_info(self):
+        print(f"Primary network =\n{self.q_network_1}\nTarget network =\n{self.q_network_2}")
--- gym/lunar-lander/Main.py
+++ gym/lunar-lander/Main.py
@@ -1,3 +1,13 @@
 from Agent import Agent
 
-agent = Agent()
+from Approximator import Approximator
+
+# agent = Agent()
+
+approximator = Approximator()
+
+approximator.create_network_q1()
+
+approximator.create_network_q2()
+
+approximator.get_network_info()
