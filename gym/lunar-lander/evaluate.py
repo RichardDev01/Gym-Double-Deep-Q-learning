93825fcdbaaa6d738c375ad6bd1421ba40ef5d37
Jellestiesri <jellestiesri@gmail.com>
2021-12-21 12:25:38 +0100

    cleaned evaluate

--- gym/lunar-lander/evaluate.py
+++ gym/lunar-lander/evaluate.py
@@ -7,15 +7,8 @@ import gym
 
 
 def evaluate():
-    """
-    Evaluate the target model for x episodes.
+    """Evaluate the target model for x episodes."""
 
-    Args:
-        episodes:
-
-    Returns:
-
-    """
     env = gym.make("LunarLander-v2")
 
     policy = EpsilonGreedyPolicy(env, epsilon=1.0, decay_factor=0.995, minimal_epsilon=0.1)
@@ -25,10 +18,9 @@ def evaluate():
     total_actions = env.action_space.n
     observation_length = len(state)
 
-    agent = Agent(policy, alpha=0.1, tau=0.1, batchsize=10, gamma=1,
+    agent = Agent(policy, tau=0.1, batchsize=10, gamma=1,
                   model_input_size=observation_length, model_output_size=total_actions, model_middle_layer_size=256)
 
-    # agent.load_model('default_primary_name','default_primary_name')
     agent.load_model('default_target_name', 'default_target_name')
 
     while True:
